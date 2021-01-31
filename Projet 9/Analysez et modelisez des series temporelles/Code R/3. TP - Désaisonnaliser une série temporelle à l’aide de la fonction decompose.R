83012f79b536ffb0e4f38b5cefa9cd60734c4e1f
Matt <matthieulevallois1@gmail.com>
2021-01-31 11:31:21 -0500

    Parcours OpenclassRooms

--- Projet 9/Analysez et modelisez des series temporelles/Code R/3. TP - Désaisonnaliser une série temporelle à l’aide de la fonction decompose.R
+++
@@ -1,7 +0,0 @@
-###############################################################################
-# TP : Désaisonnaliser une série temporelle à l'aide de la fonction decompose
-###############################################################################
-
-# On utilise la fonction `decompose` :
-decomp.x=decompose(x,type="multiplicative")
-plot(decomp.x)
